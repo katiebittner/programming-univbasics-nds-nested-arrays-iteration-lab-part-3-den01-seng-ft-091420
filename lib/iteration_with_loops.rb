c83d2ad1a52ae5ff956ecb9a26159a6b59473213
Katie B <katiebittner1@gmail.com>
2020-08-30 19:27:22 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,11 +1,11 @@
 def join_nested_strings(src)
-  
+  joined_string = ""
   index = 0 
   while index < src.count do
     element_index = 0
     while element_index < src[index].count do 
       if src[index][element_index].instance_of?(String) 
-        p src[row_index][element_index]
+        joined_string += rc[index][element_index] += " "
       end
       element_index += 1
     end
